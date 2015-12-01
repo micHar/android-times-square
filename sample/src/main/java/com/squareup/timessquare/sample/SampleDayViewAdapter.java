70a13fba7510bd919d1b5ee3a1cb175d72d075ee
Michał Klimczak <m.klimczak@futuremind.com>
2015-12-01 12:36:02 +0100

    SampleDayViewAdapter stub

---
+++ sample/src/main/java/com/squareup/timessquare/sample/SampleDayViewAdapter.java
@@ -0,0 +1,16 @@
+package com.squareup.timessquare.sample;
+
+import com.squareup.timessquare.CalendarCellView;
+import com.squareup.timessquare.DayViewAdapter;
+
+/**
+ * Created by Michal on 01/12/15.
+ */
+public class SampleDayViewAdapter implements DayViewAdapter {
+
+    @Override
+    public void makeCellView(CalendarCellView parent) {
+
+    }
+
+}
